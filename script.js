01f67fafcae43a7788d4d07f34049e1bc706ff01
Cheris Quessou <quessoucheris144@gmail.com>
2024-06-13 11:36:39 +0100

    Added Javascript and finished html and css

---
+++ script.js
@@ -0,0 +1,15 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrtsuvwxyz"
+const number = "0123456789"
+const symbol = "!£$%^&*()-_=+[]{}@#~<>?/"
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+}
