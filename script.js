335b47a52f324531e30fd35e0a6f5ae9b718207a
Cheris Quessou <quessoucheris144@gmail.com>
2024-06-14 11:21:02 +0100

    Finished All Code

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ const lowerCase = "abcdefghijklmnopqrtsuvwxyz"
 const number = "0123456789"
 const symbol = "!£$%^&*()-_=+[]{}@#~<>?/"
 
+const allChars = upperCase + lowerCase + number + symbol;
+
 function createPassword(){
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += number[Math.floor(Math.random() * number.length)];
     password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)]
+    }
+
+    passwordBox.value = password;
+
+    function copyPassword() {
+        var copyText = document.getElementById("password");
+      
+        copyText.select();
+      
+        navigator.clipboard.writeText(copyText.value);
+        
+        alert("Copied the text: " + copyText.value);
+      }
 }
